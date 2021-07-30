583755ac409bc68c9555f4d5d6cd29ad865e0cae
beck666666 <72568665+beck666666@users.noreply.github.com>
2021-07-30 13:43:59 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+
+require('dotenv').config();
+
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+	options: { debug: true, messagesLogLevel: "info" },
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: process.env.TWITCH_BOT_USERNAME,
+		password: process.env.TWITCH_OAUTH_TOKEN
+	},
+	channels: [ 'sadbeck' ]
+});
+client.connect().catch(console.error);
+
+client.on('message', (channel, tags, message, self) => {
+	if(self) return;
+	if(message.toLowerCase() === '!hello') {
+		client.say(channel, `@${tags.username}, opa iai parça`);
+	}
+
+});
