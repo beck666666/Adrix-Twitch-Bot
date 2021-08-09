52f88bb42f6e42d7cb46fe4b0f0c08d69e8666e0
beck666666 <72568665+beck666666@users.noreply.github.com>
2021-08-09 20:31:48 -0300

    Update listas.js

--- listas.js
+++ listas.js
@@ -1,5 +1,6 @@
 let ListasEspeciais={
-    streamersRecomendados: ['RoubandoALua','LoubackLavoura','sadbeck'],
+    streamersRecomendados: ['cafecomaquarela','RoubandoALua','LoubackLavoura','sadbeck','pedrocl2001','elwarart','wendymoon7','anaconesa','AndyAllex','DimiPim','rocketfalls','ilustreilustrador','EstudioSuzume'
+,'EstudioSuzume','Caravaggiobaiano','Lampjor','Choconiih'],
     oponente1:[],
     oponente2:[],
     gameStat:0,
