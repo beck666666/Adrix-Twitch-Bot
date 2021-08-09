bdc5bd90592693ee621a52771ec7c0ab6998550c
beck666666 <72568665+beck666666@users.noreply.github.com>
2021-08-09 20:10:43 -0300

    Create listas.js

---
+++ listas.js
@@ -0,0 +1,16 @@
+let ListasEspeciais={
+    streamersRecomendados: ['RoubandoALua','LoubackLavoura','sadbeck'],
+    oponente1:[],
+    oponente2:[],
+    gameStat:0,
+    pedraPapelTesoura:['pedra','papel','tesoura'],
+    op1jogada:[],
+    op2jogada:[],
+    op1Pontos:0,
+    op2Pontos:0,
+    round:0,
+    melhorDe:10,
+    
+};
+
+module.exports = ListasEspeciais
