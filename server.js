1122a8aa3fac2fd2d9766edeb80be65f4722479a
beck666666 <72568665+beck666666@users.noreply.github.com>
2021-09-08 21:16:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ client.on('message', (channel, tags, message, self,) => {
 		'seu nome é hugo? pq vc é hugostosao']},
 		
 		
-		{chamadas: ['comandos especiais', '!comandos especiais'], respostas: [`o comando:!colocar foto PogChamp , este comando exige um emote no final ele vai colocar o emote que vc escolheu como seu avatar no chat da tela, tambem tem o comando para mudar as cores do overlay, esse comando so pode ser ultilizado com a recompensa de canal "destacar minha mensagem" e digitar uma cor valida, as cores validas são essas https://pt.wikipedia.org/wiki/Lista_de_cores digite o nome da cor com a primeira letra maiuscula e sem !!!`]}
+		{chamadas: ['comandos especiais', '!comandos especiais'], respostas: [`o comando:!colocar foto PogChamp , este comando exige um emote no final ele vai colocar o emote que vc escolheu como seu avatar no chat da tela, tambem tem o comando para mudar as cores do overlay, esse comando so pode ser ultilizado com a recompensa de canal "destacar minha mensagem" e digitar uma cor valida, as cores validas são essas https://pt.wikipedia.org/wiki/Lista_de_cores digite o nome da cor com a primeira letra maiuscula e sem !!!`]},
 		
 		{chamadas: ['comandos', 'bot'], respostas: [`aqui esta alguns dos comandos: rolard20, rolard6, piada, instagram, discord, comandos especiais. Esses são apenas alguns dos meus comandos, e varios deles possuem variações tanto para chamar quando para as respostas, tambem exitem outros comandos secretos mas vcs vão ter que descobrir ai, se vira, exite tambem os comandos especiais da uma olhada neles ai`]},
 		
